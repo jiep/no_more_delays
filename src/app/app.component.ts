cbdcef9f9f0e88011031fac035254ee2dc700d13
José Ignacio Escribano <jiep12345@hotmail.com>
2017-09-08 20:25:19 +0200

    Added Bulma and first mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+  delays: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.delays = af.list('/delays', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.user = this.afAuth.authState;
+  }
+
+  loginWithGoogle() {
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  send(delay: string) {
+      let uid;
+      this.delays.push({date: new Date(), delay: delay, line: 'L1', station: 'Sol' });
+      this.msgVal = '';
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireOfflineModule } from 'angularfire2-offline';
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCbrBO6xv0x9Wz3c1NC2DNgzxd26rYmtrs",
+    authDomain: "no-more-delays.firebaseapp.com",
+    databaseURL: "https://no-more-delays.firebaseio.com",
+    projectId: "no-more-delays",
+    storageBucket: "no-more-delays.appspot.com",
+    messagingSenderId: "359132548555"
+  };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireOfflineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
